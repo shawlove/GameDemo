155cb6f75d9075d954846695f8945bdb78fe9d94
shawlove <31810880+shawlove@users.noreply.github.com>
2018-07-11 19:37:29 +0800

    导入项目

---
+++ Game/Assets/script/battle/enemy.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+        }
+    }
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;
+        }
+    }
+    public string Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+        }
+    }
+    public bool IsAttacked
+    {
+        get
+        {
+            return isAttacked;
+        }
+        set
+        {
+            isAttacked = value;
+        }
+    }
+    public GameObject PlayerObj
+    {
+        get
+        {
+            return playerObj;
+        }
+        set
+        {
+            playerObj = value;
+        }
+    }
+    public Vector3 _position;
+    private GameObject playerObj;
+    private bool isAttacked;
+    private int hp;
+    private int maxHp=100;
+    private string _name;
+    private int id;
+    private string type;
+    private RectTransform blood;
+    private Animation anim;
+    private NavMeshAgent navMeshAgent;
+    private float toOther;
+    private float attackDistance;
+    private float range;
+    //private bool isYield;
+    private ParticleSystem effects;
+    //private int count;//用来判断第一次被击中后，需要延迟一会再攻击；
+    private float rotationSpeed=1f;
+    private float coolingTime;
+    private float coolTime;
+
+    void Start()
+    {
+        _position = new Vector3(-79,0.1f,40);
+        range = 15f;
+        attackDistance = 5f;
+        hp = maxHp;
+        blood = transform.FindChild("EnemyStatu/Panel/EnemyBloodStrip/EnemyBlood").GetComponent<RectTransform>();
+        anim = GetComponent<Animation>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        effects = transform.FindChild("AuraActivateLight").GetComponent<ParticleSystem>();
+        anim["touchHead"].speed = 2;
+        playerObj = null;
+        coolingTime = 0;
+        coolTime = 2.5f;
+       // isYield = false;
+       // count = 0;
+    }
+
+    void Update()
+    {
+        coolingTime += Time.deltaTime;
+        blood.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left,0.007f,hp*0.00192f);
+        if ((transform.position-_position).magnitude>=range)
+        {            
+            if (playerObj!=null)
+            {
+                playerObj = null;//失去目标
+                //StopAllCoroutines();
+               // count = 0;
+            }           
+            navMeshAgent.SetDestination(_position);
+        }
+        if (playerObj!=null)
+        {
+             toOther = (playerObj.transform.position - transform.position).magnitude;
+            playerBattle battle = playerObj.GetComponent<playerBattle>();
+            if (toOther<=attackDistance)
+            {
+                navMeshAgent.ResetPath();
+                //if (!isYield)
+               // {
+                    StartCoroutine(waitAttack(battle));
+                   // isYield = true;
+               // }
+            }else
+            {
+                navMeshAgent.SetDestination(playerObj.transform.position);
+                StopAllCoroutines();
+               // isYield = false;
+            }
+        }else
+        {
+            if ((transform.position - _position).magnitude <= 1)
+            {
+                navMeshAgent.ResetPath();
+            }
+        }
+        if (navMeshAgent.remainingDistance>3f)
+        {
+            anim.Play("run",PlayMode.StopSameLayer);
+        }
+        if (isAttacked)
+        {
+            anim.Play("touchHead",PlayMode.StopSameLayer);
+            isAttacked = false;
+        }
+        anim.PlayQueued("idle01",QueueMode.CompleteOthers,PlayMode.StopSameLayer);
+        if (hp<=0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+    IEnumerator waitAttack(playerBattle battle)
+    {
+       
+        //if (count==0)
+        //{
+        //    count = 1;
+        //    yield return new WaitForSeconds(interval);           
+        //}
+        float dot = 0.5f;
+        int _damage = 20;
+        float _dot = 0;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 v = new Vector3(transform.position.x, playerObj.transform.position.y, transform.position.z);
+        Vector3 _toOther = playerObj.transform.position - v;
+        _dot = Vector3.Dot(forward, _toOther);
+        if (_dot< dot * attackDistance)
+        {
+            transform.rotation = Quaternion.Slerp(Quaternion.LookRotation(forward), Quaternion.LookRotation(playerObj.transform.position - transform.position), rotationSpeed * Time.deltaTime);
+            
+        }
+        else
+        {
+            if (coolingTime>=coolTime)
+            {
+                coolingTime = 0;
+                anim.Play("choice", PlayMode.StopAll);
+                effects.Play();
+                yield return new WaitForSeconds(0.3f);
+                if (dot * attackDistance < _dot)
+                {
+                    battle.CurrntHp -= _damage;
+                }
+            }
+            
+        }      
+        //StartCoroutine(waitAttack(interval, battle));
+    }
+
+
+
+	}
+
---
+++ Game/Assets/script/playerAnim.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class playerAnim : MonoBehaviour {
+
+   
+    private fps_playerParamter paramter;
+    private  Animator anim;
+    private float random;
+    private Vector2 currentMove = Vector2.zero;
+    private bool currentAttack01 = false;
+    private bool currentAttack02 = false;
+    private GameObject[] skillBars;
+    private int skillState;
+    public Dictionary<int, float> cooling = new Dictionary<int, float>();
+    public float[] cools=new float[10];
+
+
+    void Start()
+    {
+        paramter = this.GetComponent<fps_playerParamter>();
+        anim = this.GetComponent<Animator>();
+ 
+    }
+
+
+    void Update()
+    {
+        animControll();
+        skillBarDown();
+        foreach (KeyValuePair<int,float> pair in cooling)
+        {
+            if (cools[pair.Key]<pair.Value)
+            {
+                cools[pair.Key] += Time.deltaTime;
+            }
+            
+        }
+        
+    }
+    
+    public void skillAnimEvent()
+    {
+        anim.SetInteger("skill",0);
+    }
+
+    private void skillBarDown()
+    {
+        skillBars = GameObject.FindGameObjectsWithTag("skillBar");
+        if (paramter.isSkillbar01)
+        {
+            skillState = skillBars[0].GetComponentInChildren<skill>().SkillState;
+            if (cooling.ContainsKey(skillBars[0].GetComponentInChildren<skill>().Id))
+            {
+                if (cools[skillBars[0].GetComponentInChildren<skill>().Id]>=skillBars[0].GetComponentInChildren<skill>().CoolTime)
+                {
+                    anim.SetInteger("skill", skillState);
+                    
+                    cools[skillBars[0].GetComponentInChildren<skill>().Id] = 0;
+                }
+            }
+            else
+            {
+                anim.SetInteger("skill", skillState);
+                cooling[skillBars[0].GetComponentInChildren<skill>().Id] = skillBars[0].GetComponentInChildren<skill>().CoolTime;
+                cools[skillBars[0].GetComponentInChildren<skill>().Id] = 0;
+                //skillBars[0].GetComponentInChildren<Image>().color = new Color(194/255,194/255,194/255);
+            }
+            
+        }
+        if (paramter.isSkillbar02)
+        {
+            skillState = skillBars[1].GetComponentInChildren<skill>().SkillState;
+            anim.SetInteger("skill", skillState);
+            if (cooling.ContainsKey(skillBars[1].GetComponentInChildren<skill>().Id))
+            {
+                if (cools[skillBars[1].GetComponentInChildren<skill>().Id] >= skillBars[1].GetComponentInChildren<skill>().CoolTime)
+                {
+                    anim.SetInteger("skill", skillState);
+
+                    cools[skillBars[1].GetComponentInChildren<skill>().Id] = 0;
+                }
+            }
+            else
+            {
+                anim.SetInteger("skill", skillState);
+                cooling[skillBars[1].GetComponentInChildren<skill>().Id] = skillBars[1].GetComponentInChildren<skill>().CoolTime;
+                cools[skillBars[1].GetComponentInChildren<skill>().Id] = 0;
+                //skillBars[1].GetComponentInChildren<Image>().color = new Color(194 / 255, 194 / 255, 194 / 255);
+            }
+        }
+        if (paramter.isSkillbar03)
+        {
+            skillState = skillBars[2].GetComponentInChildren<skill>().SkillState;
+            anim.SetInteger("skill", skillState);
+            if (cooling.ContainsKey(skillBars[2].GetComponentInChildren<skill>().Id))
+            {
+                if (cools[skillBars[2].GetComponentInChildren<skill>().Id] >= skillBars[2].GetComponentInChildren<skill>().CoolTime)
+                {
+                    anim.SetInteger("skill", skillState);
+
+                    cools[skillBars[2].GetComponentInChildren<skill>().Id] = 0;
+                }
+            }
+            else
+            {
+                anim.SetInteger("skill", skillState);
+                cooling[skillBars[2].GetComponentInChildren<skill>().Id] = skillBars[2].GetComponentInChildren<skill>().CoolTime;
+                cools[skillBars[2].GetComponentInChildren<skill>().Id] = 0;
+               // skillBars[2].GetComponentInChildren<Image>().color = new Color(194 / 255, 194 / 255, 194 / 255);
+            }
+        }
+        if (paramter.isSkillbar04)
+        {
+            skillState = skillBars[3].GetComponentInChildren<skill>().SkillState;
+            anim.SetInteger("skill", skillState);
+            if (cooling.ContainsKey(skillBars[3].GetComponentInChildren<skill>().Id))
+            {
+                if (cools[skillBars[3].GetComponentInChildren<skill>().Id] >= skillBars[3].GetComponentInChildren<skill>().CoolTime)
+                {
+                    anim.SetInteger("skill", skillState);
+
+                    cools[skillBars[3].GetComponentInChildren<skill>().Id] = 0;
+                }
+            }
+            else
+            {
+                anim.SetInteger("skill", skillState);
+                cooling[skillBars[3].GetComponentInChildren<skill>().Id] = skillBars[3].GetComponentInChildren<skill>().CoolTime;
+                cools[skillBars[3].GetComponentInChildren<skill>().Id] = 0;
+               // skillBars[3].GetComponentInChildren<Image>().color = new Color(194 / 255, 194 / 255, 194 / 255);
+            }
+        }
+        //这里想用触发器做，，这样感觉很蠢。以后来改
+    }
+
+    private void animControll()
+    {
+        
+        currentMove.x = paramter.inputMoveVector.x;
+        currentMove.y = paramter.inputMoveVector.y;
+
+        currentAttack01 = paramter.inputAttack01;
+        currentAttack02 = paramter.inputAttack02;
+        anim.SetFloat("turn",currentMove.x);
+        anim.SetFloat("forward", currentMove.y);
+       
+        if (currentAttack01)
+        {
+
+            if (Random.Range(0f, 10f) < 3f)
+            {
+                anim.SetInteger("state", 3);
+            }
+            else
+            {
+                anim.SetInteger("state", 1);
+            }
+
+        }
+        else
+        {
+            anim.SetInteger("state", 0);
+        }
+       
+        if (currentAttack02)
+        {
+            
+            if (Random.Range(0f, 10f) < 3f)
+            {
+                anim.SetInteger("state", 4);
+            }
+            else
+            {
+                anim.SetInteger("state", 2);
+            }
+            
+           
+        }
+        else
+        {
+            anim.SetInteger("state", 0);
+        }
+
+        if (paramter.isBattle)
+        {
+            anim.SetFloat("isBattle",0);
+        }else
+        {
+            if (currentMove == Vector2.zero)
+            {
+                
+        
+                anim.SetFloat("isBattle", 1);
+                
+            }
+
+            anim.SetFloat("isBattle", 0);
+
+        }
+
+
+    }
+   
+    
+
+
+}
